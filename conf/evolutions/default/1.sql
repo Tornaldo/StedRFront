e2c3c5c9871ac7556a0642cb87483f989def966f
Christian Frøystad <christian@xist.no>
2013-10-05 15:06:03 +0200

    Adds report to repository

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -9,7 +9,15 @@ create table wall (
   constraint pk_wall primary key (id))
 ;
 
+create table wall_model (
+  id                        bigint not null,
+  name                      varchar(255),
+  constraint pk_wall_model primary key (id))
+;
+
 create sequence wall_seq;
+
+create sequence wall_model_seq;
 
 
 
@@ -18,5 +26,9 @@ create sequence wall_seq;
 
 drop table if exists wall cascade;
 
+drop table if exists wall_model cascade;
+
 drop sequence if exists wall_seq;
+
+drop sequence if exists wall_model_seq;
 
