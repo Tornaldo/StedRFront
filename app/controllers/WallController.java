05ab9c49b4bfd7f739755362cc4976e65bdc749f
simonstastny <stastny.simon@gmail.com>
2013-10-09 20:51:58 +0200

    getting rid of diamond syntax in wall model

--- app/controllers/WallController.java
+++ app/controllers/WallController.java
@@ -8,8 +8,17 @@ import play.mvc.Result;
 
 public class WallController extends Controller {
 	
-	public static Result listWallsInJson() {
+	public static Result listAllWalls() {
 		List<WallModel> walls = WallModel.findAll();
 		return ok(Json.toJson(walls));
 	}
+	
+	public static Result listWallsForLocation(Double latitude, Double longitude) {
+		System.out.println("lat: " + latitude + " long: " + longitude);
+		
+		List<WallModel> walls = WallModel.findAll();
+		return ok(Json.toJson(walls));
+	}
+	
+	
 }
