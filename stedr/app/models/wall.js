437e0a69285d380b05bde198597686bf4b559077
Christian Frøystad <christian@xist.no>
2013-10-10 14:27:08 +0200

    More work on models for walls

--- stedr/app/models/wall.js
+++ stedr/app/models/wall.js
@@ -1,12 +1,28 @@
 exports.definition = {
 	config : {
-		"columns"	: {},
+		"columns"	: {
+			"wallId" 		: "int",
+			"title" 		: "string",
+			"description"	: "string",
+			"latitude"		: "double",
+			"longitude"		: "double",
+		},
 		"URL" 		: "",
 		"adapter" 	: {
 			"type"	: "restapi",
 			"collection_name" : "wallsCollection",
-			"idAttribute" : "id" 
+			"idAttribute" : "wallId" 
 		}
 	},
-	
+	extendModel : function(Model) {
+		_.extend(Model.prototype, {
+			// If adding and editing walls shall be allowed, the specific url must be added here
+		});
+		return Model;
+	},
+	extendCollection : function(Collection) {
+		_.extend(Collection.prototype, {});
+		return Collection;
+	}
 };
+
