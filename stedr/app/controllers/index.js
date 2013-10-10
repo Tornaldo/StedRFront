2e6604e3aab184b44c7e7f396e8fad0c2fbb7816
Christian Frøystad <christian@xist.no>
2013-10-10 17:08:08 +0200

    Adds basic table UI for walls

--- stedr/app/controllers/index.js
+++ stedr/app/controllers/index.js
@@ -1,5 +1,32 @@
-function doClick(e) {
-    alert($.label.text);
+$.index.open();
+
+
+function transformFunction(model) {
+	Ti.API.log(model);
+	var transform = model.toJSON();
+	transform.title = '[' + transform.title + ']';
+	return transform;
+};
+
+function filterFunction(collection) {
+	return collection;
 }
 
-$.index.open();
+var wall = Alloy.Collections.wall;
+wall.fetch({
+	success : function(){
+		// Here we goes through all the models, so any custom logic should be done here
+        _.each(wall.models, function(element, index, list){
+            
+        });
+        Ti.API.log(wall);
+    },
+    error : function(){
+        Ti.API.error("hmm - this is not good!");
+    }
+});
+
+
+$.index.addEventListener('close', function() {
+	$.destroy();
+});
