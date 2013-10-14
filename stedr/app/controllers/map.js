e4166f17a58af0475e236fff6cdca0c9a704a813
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-10-14 17:34:40 +0200

    pins from the DB appears

--- stedr/app/controllers/map.js
+++ stedr/app/controllers/map.js
@@ -10,7 +10,7 @@ var wallTitle = null;
 
 mapview.addEventListener('click', function(evt) {
 	Ti.API.info("Annotation " + evt.title);
-	if (evt.clicksource == 'leftPane') {
+	if (evt.clicksource == 'leftButton') {
 		var wall = Alloy.createController('stedrWall').getView();
 		wall.open();
 	}
@@ -18,40 +18,26 @@ mapview.addEventListener('click', function(evt) {
 
 function createAnnotations() {
 	var wallList = new Array();
-	// var q = Alloy.Collections.wall;
-	// q.fetch();
-	// Ti.API.info(q);
-	// for (var i in q) {
-		// var mapAnnotation = Titanium.Map.createAnnotation({
-			// latitude : i.latitude,
-			// longitude : i.longitude,
-			// pincolor : Titanium.Map.ANNOTATION_GREEN
-		// });
-		// Ti.API.info(mapAnnotation);
-		// Ti.API.info(i.name);
-		// wallList.push(mapAnnotation);
-	// }
-
-	var x = Titanium.Map.createAnnotation({
-		latitude : 52.702187,
-		longitude : 10.228271,
-		title : "Somewhere in Deutschland",
-		pincolor : Titanium.Map.ANNOTATION_RED,
-		leftButton : "/images/buttonimage.jpg",
+	var walls = Alloy.Collections.wall;
+	walls.fetch({
+		success : function() {
+			// Here we goes through all the models, so any custom logic should be done here
+			_.each(walls.models, function(element, index, list) {
+				Ti.API.info(element.get('latitude'));
+				var mapAnnotation = Titanium.Map.createAnnotation({
+					title : element.get('name'),
+					latitude : element.get('latitude'),
+					longitude : element.get('longitude'),
+					pincolor : Titanium.Map.ANNOTATION_GREEN,
+					leftButton : "/images/buttonimage.jpg"
+				});
+				Ti.API.info(mapAnnotation);
+				wallList.push(mapAnnotation);
+			});
+			mapview.annotations = wallList;
+		},
+		error : function() {
+			Ti.API.error("hmm - this is not good!");
+		}
 	});
-	var y = Titanium.Map.createAnnotation({
-		latitude : 63.428283,
-		longitude : 10.395041,
-		title : "Trondheim",
-		pincolor : Titanium.Map.ANNOTATION_RED,
-		leftButton : "/images/buttonimage.jpg",
-		
-	});
-	wallList.push(x);
-	wallList.push(y);
-	Ti.API.info(wallList.length);
-	for (var i in wallList) {
-		Ti.API.info(i.title);
-	}
-	mapview.annotations = wallList;
 }
