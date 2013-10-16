bc91113651ad6e26f6da6b04bd63ceeaf63e7624
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-10-16 12:41:02 +0200

    messagepassing between mapcontr. and wallcontr.

--- stedr/app/controllers/stedrWall.js
+++ stedr/app/controllers/stedrWall.js
@@ -1,2 +1,17 @@
-function setUp(){
+var args = arguments[0] || {};
+
+$.wallModel = _.extend({}, $.wallModel, {
+	transform : function() {
+		return dataTransformation(this);
+	}
+});
+
+$.wallModel.set(args.data || {});
+
+function dataTransformation(_model) {
+	Ti.API.info(_model.get('name'));
+	return {
+		name : _model.get('name')
+	};
 }
+
