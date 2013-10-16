02a41dfd7cd27e9e8e9775524b2ddb0fc9d83e86
Christian Frøystad <christian@xist.no>
2013-10-16 16:08:04 +0200

    Ingnores only Report.pdf, not all PDF files

--- README.md
+++
@@ -1,4 +0,0 @@
-VirtualWall
-===========
-
-TDT4290 Customer Driven Project for SINTEF &amp; STFK
--- stedr/Resources/app.js
+++
@@ -1,5 +0,0 @@
-var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
-
-Alloy.Globals.baseServerUrl = "http://localhost:9000";
-
-Alloy.createController("index");
