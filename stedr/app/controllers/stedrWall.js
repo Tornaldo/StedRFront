ae420d26b24cbdf2f91d42d925186a8d32ffcd25
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-10-18 12:35:14 +0200

    access wall with picture gallery, working

--- stedr/app/controllers/stedrWall.js
+++ stedr/app/controllers/stedrWall.js
@@ -1,1 +1,20 @@
 var args = arguments[0] || {};
+
+var data = $model;
+
+Ti.API.info("Entering: " + data.get('name'));
+
+
+var imageUrlList = data.get('pictures');
+
+for(i = 0; i < imageUrlList.length; i++) {
+	var wallImage = Ti.UI.createImageView({
+		image : imageUrlList[i].url
+	});
+	$.imageScroller.addView(wallImage);
+}
+
+$.stedrWall.addEventListener('close', function() {
+	Ti.API.info("Destroying: " + data.get('name'));
+	$.destroy();
+});
