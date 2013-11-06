4169e807eefaa3e07757559060a5225c51cfaa70
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-11-06 17:43:26 +0100

    bug fix, stories without videos and/or images

--- stedr/app/controllers/mediaGallery.js
+++ stedr/app/controllers/mediaGallery.js
@@ -1,7 +1,7 @@
 /**
  *	Copyright (c) 2013, Odd Fredrik Rogstad, Christian Frøystad, Simon Stastny, Knut Nergård
  *	All rights reserved.
- *	
+ *
  *	Redistribution and use in source and binary forms, with or without
  *	modification, are permitted provided that the following conditions are met:
  *	* Redistributions of source code must retain the above copyright
@@ -9,10 +9,10 @@
  *	* Redistributions in binary form must reproduce the above copyright
  *	  notice, this list of conditions and the following disclaimer in the
  *	  documentation and/or other materials provided with the distribution.
- *	* Neither the name of the project nor the 
+ *	* Neither the name of the project nor the
  *	  names of its contributors may be used to endorse or promote products
  *	  derived from this software without specific prior written permission.
- *	
+ *
  *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,7 +22,7 @@
  *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 var imageUrlList = $model.get('pictures');
@@ -30,67 +30,71 @@ var videoUrlList = $model.get('videos');
 
 Ti.API.info("Media for: " + $model.get('title'));
 
-for ( i = 0; i < imageUrlList.length; i++) {
-	var wallImage = Ti.UI.createImageView({
-		image : imageUrlList[i]
-	});
-	$.mediaScroller.addView(wallImage);
+if (imageUrlList) {
+	for ( i = 0; i < imageUrlList.length; i++) {
+		var wallImage = Ti.UI.createImageView({
+			image : imageUrlList[i]
+		});
+		$.mediaScroller.addView(wallImage);
+	}
 }
 
 var deviceHeight = Ti.Platform.displayCaps.platformHeight;
 
-for ( i = 0; i < videoUrlList.length; i++) {
-	var wallVideo1 = Ti.Media.createVideoPlayer({
-		url : videoUrlList[i],
-		// backgroundColor : 'blue',
-		mediaControlStyle : Ti.Media.VIDEO_CONTROL_DEFAULT,
-		// scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
-		autoplay : true,
-	});
+if (videoUrlList) {
+	for ( i = 0; i < videoUrlList.length; i++) {
+		var wallVideo1 = Ti.Media.createVideoPlayer({
+			url : videoUrlList[i],
+			// backgroundColor : 'blue',
+			mediaControlStyle : Ti.Media.VIDEO_CONTROL_DEFAULT,
+			// scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
+			autoplay : true,
+		});
 
-	// var wallVideo2 = Titanium.Media.createVideoPlayer({
+		// var wallVideo2 = Titanium.Media.createVideoPlayer({
 		// url : videoUrlList[i],
 		// backgroundColor : 'blue',
 		// movieControlStyle : Ti.Media.VIDEO_CONTROL_DEFAULT,
 		// scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FIT,
 		// autoplay : true,
-	// });
-	// var wallVideo3 = Ti.Media.createVideoPlayer({
+		// });
+		// var wallVideo3 = Ti.Media.createVideoPlayer({
 		// url : videoUrlList[i],
 		// backgroundColor : 'blue',
 		// movieControlMode : Ti.Media.VIDEO_CONTROL_DEFAULT,
 		// scalingMode : Titanium.Media.VIDEO_SCALING_MODE_FILL,
 		// autoplay : true,
-	// });
-	// var wallVideo4 = Ti.Media.createVideoPlayer({
+		// });
+		// var wallVideo4 = Ti.Media.createVideoPlayer({
 		// url : videoUrlList[i],
 		// backgroundColor : 'blue',
 		// mediaControlStyle : Ti.Media.VIDEO_CONTROL_EMBEDDED,
 		// // scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
 		// autoplay : true,
-	// });
-	// var wallVideo5 = Ti.Media.createVideoPlayer({
+		// });
+		// var wallVideo5 = Ti.Media.createVideoPlayer({
 		// url : videoUrlList[i],
 		// backgroundColor : 'blue',
 		// movieControlStyle : Ti.Media.VIDEO_CONTROL_VOLUME_ONLY,
 		// // scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
 		// autoplay : true,
-	// });
-	// var wallVideo6 = Ti.Media.createVideoPlayer({
+		// });
+		// var wallVideo6 = Ti.Media.createVideoPlayer({
 		// url : videoUrlList[i],
 		// backgroundColor : 'blue',
 		// movieControlMode : Ti.Media.VIDEO_CONTROL_VOLUME_ONLY,
 		// // scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
 		// autoplay : true,
-// 
-	// }); 
+		//
+		// });
 
-	$.mediaScroller.addView(wallVideo1);
-	// $.mediaScroller.addView(wallVideo2);
-	// $.mediaScroller.addView(wallVideo3);
-	// $.mediaScroller.addView(wallVideo4);
-	// $.mediaScroller.addView(wallVideo5);
-	// $.mediaScroller.addView(wallVideo6);
+		$.mediaScroller.addView(wallVideo1);
+		// $.mediaScroller.addView(wallVideo2);
+		// $.mediaScroller.addView(wallVideo3);
+		// $.mediaScroller.addView(wallVideo4);
+		// $.mediaScroller.addView(wallVideo5);
+		// $.mediaScroller.addView(wallVideo6);
+	}
 }
 
 $.mediaScroller.addEventListener('close', function() {
