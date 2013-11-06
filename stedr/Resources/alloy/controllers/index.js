c4a8eeb458adc6d76ffaf39a46346420ebaaf41f
Christian Frøystad <christian@xist.no>
2013-11-06 20:19:05 +0100

    Git is forcing me to commit the ghost files...

--- stedr/Resources/alloy/controllers/index.js
+++ stedr/Resources/alloy/controllers/index.js
@@ -37,13 +37,18 @@ function Controller() {
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.mapWin.open();
-    var MapModule;
     var mapview;
-    MapModule = require("ti.map");
-    mapview = MapModule.createView({
-        userLocation: true,
+    mapview = Titanium.Map.createView({
+        mapType: Titanium.Map.STANDARD_TYPE,
         animate: true,
-        mapType: MapModule.NORMAL_TYPE
+        regionFit: true,
+        userLocation: true,
+        region: {
+            latitude: 63.427255,
+            longitude: 10.396545,
+            latitudeDelta: .01,
+            longitudeDelta: .01
+        }
     });
     $.mapView.add(mapview);
     mapview.addEventListener("click", function(evt) {
@@ -59,48 +64,21 @@ function Controller() {
     });
     $.mapSearchButton.addEventListener("click", function() {
         var searchText = $.searchField.getValue();
-        Ti.API.info("PHONE");
-        var xhr = Titanium.Network.createHTTPClient();
-        var url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + searchText + "&sensor=true&key=AIzaSyD7QIWz-xIs3WTWYR_0eaH_whi56NNE1sE";
-        xhr.open("GET", url);
-        xhr.onload = function() {
-            var json = JSON.parse(this.responseText);
-            switch (json.status) {
-              case "OK":
-                Ti.API.info(JSON.stringify(json));
-                mapview.setLocation({
-                    latitude: json.results[0].geometry.location.lat,
-                    longitude: json.results[0].geometry.location.lng,
-                    latitudeDelta: .1,
-                    longitudeDelta: .1
-                });
-                break;
-
-              case "ZERO_RESULTS":
-                alert("No result for your search");
-                break;
-
-              case "OVER_QUERY_LIMIT":
-                alert("Sorry, the query limit is exceeded");
-                break;
-
-              case "REQUEST_DENIED":
-                alert("Sorry, your request was denied");
-                break;
-
-              case "INVALID_REQUEST":
-                alert("Sorry, your request is invalid");
-                break;
-
-              default:
-                alert("This is very strange! Do you have internet connection?");
+        Ti.API.info("WEB");
+        var geocoder = new google.maps.Geocoder();
+        geocoder ? geocoder.geocode({
+            address: searchText
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) mapview.setLocation({
+                latitude: results[0].geometry.location.lat(),
+                longitude: results[0].geometry.location.lng(),
+                latitudeDelta: .1,
+                longitudeDelta: .1
+            }); else {
+                Ti.API.error(status);
+                Ti.API.info(JSON.stringify(results));
             }
-        };
-        xhr.onerror = function(e) {
-            alert("This is very strange! Do you have internet connection?");
-            Ti.API.error(e.error);
-        };
-        xhr.send();
+        }) : alert("Google Maps Geocoder not supported");
     });
     var wallCollection = Alloy.Collections.wall;
     wallCollection.fetch({
@@ -108,14 +86,13 @@ function Controller() {
             _.each(wallCollection.models, function(element) {
                 Ti.API.info("Making annotation for " + element.get("title"));
                 var mapAnnotation;
-                var mapAnnotation = MapModule.createAnnotation({
+                var mapAnnotation = Titanium.Map.createAnnotation({
                     title: element.get("title"),
                     latitude: element.get("latitude"),
                     longitude: element.get("longitude"),
                     rightView: Ti.UI.createImageView({
                         image: element.get("thumbnailUrl")
                     }),
-                    pincolor: MapModule.ANNOTATION_AZURE,
                     id: element.get("id")
                 });
                 mapview.addAnnotation(mapAnnotation);
--- stedr/Resources/app.js
+++ stedr/Resources/app.js
@@ -1,7 +1,5 @@
 var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
 
-Alloy.Globals.Map = require("ti.map");
-
 Alloy.Collections.wall = Alloy.createCollection("wall");
 
 Alloy.Collections.story = Alloy.createCollection("story");
