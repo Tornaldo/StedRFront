093fdc18617d4b2d6142113f9a923ffe6e66de62
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-11-07 21:04:22 +0100

    resource files, android

--- stedr/Resources/alloy/styles/stedrWall.js
+++ stedr/Resources/alloy/styles/stedrWall.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = [ {
     isClass: true,
     priority: 10000.0018,
@@ -12,11 +11,10 @@ module.exports = [ {
     key: "pictureTab",
     style: {
         backgroundColor: "#8D8D8D",
-        borderColor: "#8D8D8D",
         borderRadius: 4,
         textAlign: Titanium.UI.TEXT_ALIGNMENT_CENTER,
-        color: "white",
         font: {
+            color: "white",
             fontFamily: "Helvetica",
             fontSize: "25sp",
             fontStyle: "normal",
@@ -28,10 +26,11 @@ module.exports = [ {
     priority: 100000.002,
     key: "storyTab",
     style: {
+        left: "10%",
         backgroundColor: "#40B0D2",
-        borderColor: "#8D8D8D",
         borderRadius: 4,
-        color: "white",
+        borderColor: "#40B0D2",
+        color: "#40B0D2",
         font: {
             color: "white",
             fontFamily: "Helvetica",
@@ -69,6 +68,3 @@ module.exports = [ {
         }
     }
 } ];
-=======
-module.exports = [{"isClass":true,"priority":10000.0018,"key":"container","style":{backgroundColor:"white",}},{"isId":true,"priority":100000.0019,"key":"pictureTab","style":{backgroundColor:"#8D8D8D",borderRadius:4,textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,font:{color:"white",fontFamily:"Helvetica",fontSize:"25sp",fontStyle:"normal",fontWeight:"bold",},}},{"isId":true,"priority":100000.002,"key":"storyTab","style":{left:"10%",backgroundColor:"#40B0D2",borderRadius:4,borderColor:"#40B0D2",color:"#40B0D2",font:{color:"white",fontFamily:"Helvetica",fontSize:"25sp",fontStyle:"normal",fontWeight:"bold",},}},{"isId":true,"priority":100000.0021,"key":"pictureGallery","style":{}},{"isId":true,"priority":100000.0022,"key":"tabView","style":{}},{"isId":true,"priority":100000.0023,"key":"storyAndPictureView","style":{}},{"isId":true,"priority":100000.0024,"key":"creditLabel","style":{color:"black",font:{fontFamily:"Helvetica",fontSize:"10sp",fontStyle:"normal",fontWeight:"normal",},}}];
->>>>>>> a5dba5778c167be13ff896e3e7a3534b4e7415f4
