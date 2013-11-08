efaeb4aefe4bd35e413c26db041ff8db1c8a08e3
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-11-08 22:32:55 +0100

    nothing works

--- stedr/Resources/alloy/controllers/index.js
+++ stedr/Resources/alloy/controllers/index.js
@@ -6,41 +6,15 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
-    $.__views.mapWin = Ti.UI.createWindow({
-        title: "Map",
-        id: "mapWin",
-        layout: "vertical"
-    });
-    $.__views.__alloyId0 = Ti.UI.createView({
-        height: "10%",
-        layout: "horizontal",
-        id: "__alloyId0"
-    });
-    $.__views.mapWin.add($.__views.__alloyId0);
-    $.__views.searchField = Ti.UI.createTextField({
-        id: "searchField",
-        width: "80%",
-        hintText: "Search location"
-    });
-    $.__views.__alloyId0.add($.__views.searchField);
-    $.__views.mapSearchButton = Ti.UI.createButton({
-        id: "mapSearchButton",
-        title: "Search"
-    });
-    $.__views.__alloyId0.add($.__views.mapSearchButton);
-    $.__views.mapView = Ti.UI.createView({
-        id: "mapView",
-        height: "90%"
-    });
-    $.__views.mapWin.add($.__views.mapView);
-    $.__views.nav = Ti.UI.iOS.createNavigationWindow({
-        window: $.__views.mapWin,
-        id: "nav"
-    });
-    $.__views.nav && $.addTopLevelView($.__views.nav);
     exports.destroy = function() {};
     _.extend($, $.__views);
-    "iphone" == Alloy.Globals.OS ? $.nav.open() : $.mapWin.open();
+    if ("iphone" == Alloy.Globals.OS) {
+        Ti.API.info("AD");
+        $.nav.open();
+    } else {
+        Ti.API.info("ASD");
+        $.mapWin.open();
+    }
     var MapModule;
     var mapview;
     if ("android" == Alloy.Globals.OS) {
--- stedr/Resources/alloy/styles/stedrWall.js
+++ stedr/Resources/alloy/styles/stedrWall.js
@@ -1,1 +1,75 @@
-module.exports = [{"isClass":true,"priority":10000.0018,"key":"container","style":{backgroundColor:"white",}},{"isId":true,"priority":100000.0019,"key":"pictureTab","style":{backgroundColor:"#8D8D8D",borderRadius:4,textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,font:{color:"white",fontFamily:"Helvetica",fontSize:"25sp",fontStyle:"normal",fontWeight:"bold",},}},{"isId":true,"priority":100000.002,"key":"storyTab","style":{left:"10%",backgroundColor:"#40B0D2",borderRadius:4,borderColor:"#40B0D2",color:"#40B0D2",font:{color:"white",fontFamily:"Helvetica",fontSize:"25sp",fontStyle:"normal",fontWeight:"bold",},}},{"isId":true,"priority":100000.0021,"key":"pictureGallery","style":{}},{"isId":true,"priority":100000.0022,"key":"tabView","style":{}},{"isId":true,"priority":100000.0023,"key":"storyAndPictureView","style":{}},{"isId":true,"priority":100000.0024,"key":"creditLabel","style":{color:"black",font:{fontFamily:"Helvetica",fontSize:"10sp",fontStyle:"normal",fontWeight:"normal",},}},{"isId":true,"priority":100000.0025,"key":"storyOrPictureView","style":{}}];
+module.exports = [ {
+    isClass: true,
+    priority: 10000.0018,
+    key: "container",
+    style: {
+        backgroundColor: "white"
+    }
+}, {
+    isId: true,
+    priority: 100000.0019,
+    key: "pictureTab",
+    style: {
+        backgroundColor: "#8D8D8D",
+        borderRadius: 4,
+        textAlign: Titanium.UI.TEXT_ALIGNMENT_CENTER,
+        font: {
+            color: "white",
+            fontFamily: "Helvetica",
+            fontSize: "25sp",
+            fontStyle: "normal",
+            fontWeight: "bold"
+        }
+    }
+}, {
+    isId: true,
+    priority: 100000.002,
+    key: "storyTab",
+    style: {
+        left: "10%",
+        backgroundColor: "#40B0D2",
+        borderRadius: 4,
+        borderColor: "#40B0D2",
+        color: "#40B0D2",
+        font: {
+            color: "white",
+            fontFamily: "Helvetica",
+            fontSize: "25sp",
+            fontStyle: "normal",
+            fontWeight: "bold"
+        }
+    }
+}, {
+    isId: true,
+    priority: 100000.0021,
+    key: "pictureGallery",
+    style: {}
+}, {
+    isId: true,
+    priority: 100000.0022,
+    key: "tabView",
+    style: {}
+}, {
+    isId: true,
+    priority: 100000.0023,
+    key: "storyAndPictureView",
+    style: {}
+}, {
+    isId: true,
+    priority: 100000.0024,
+    key: "creditLabel",
+    style: {
+        color: "black",
+        font: {
+            fontFamily: "Helvetica",
+            fontSize: "10sp",
+            fontStyle: "normal",
+            fontWeight: "normal"
+        }
+    }
+}, {
+    isId: true,
+    priority: 100000.0025,
+    key: "storyOrPictureView",
+    style: {}
+} ];
