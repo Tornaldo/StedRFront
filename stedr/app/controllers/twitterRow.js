5e7315bb81a61bd498d4e54e2dc04d1e8e15c8e5
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-11-18 18:48:21 +0100

    don't know why twitter is WORKING now

--- stedr/app/controllers/twitterRow.js
+++ stedr/app/controllers/twitterRow.js
@@ -1,44 +1,50 @@
 /**
- *	Copyright (c) 2013, Odd Fredrik Rogstad, Christian Frøystad, Simon Stastny, Knut Nergård
- *	All rights reserved.
- *	
- *	Redistribution and use in source and binary forms, with or without
- *	modification, are permitted provided that the following conditions are met:
- *	* Redistributions of source code must retain the above copyright
- *	  notice, this list of conditions and the following disclaimer.
- *	* Redistributions in binary form must reproduce the above copyright
- *	  notice, this list of conditions and the following disclaimer in the
- *	  documentation and/or other materials provided with the distribution.
- *	* Neither the name of the project nor the 
- *	  names of its contributors may be used to endorse or promote products
- *	  derived from this software without specific prior written permission.
- *	
- *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *        Copyright (c) 2013, Odd Fredrik Rogstad, Christian Frøystad, Simon Stastny, Knut Nergård
+ *        All rights reserved.
+ *
+ *        Redistribution and use in source and binary forms, with or without
+ *        modification, are permitted provided that the following conditions are met:
+ *        * Redistributions of source code must retain the above copyright
+ *          notice, this list of conditions and the following disclaimer.
+ *        * Redistributions in binary form must reproduce the above copyright
+ *          notice, this list of conditions and the following disclaimer in the
+ *          documentation and/or other materials provided with the distribution.
+ *        * Neither the name of the project nor the
+ *          names of its contributors may be used to endorse or promote products
+ *          derived from this software without specific prior written permission.
+ *
+ *        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *        ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *        WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *        DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *        (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *        ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-Alloy.Collections.tweets.reset($model);	
+Alloy.Collections.tweets.reset($model);
 
 function transformFunction(model) {
-    // Need to convert the model to a JSON object
-    var transform = model.toJSON();
-    transform.userName = transform.user.name;
-    transform.screenName = "@" + transform.user.screen_name;
-    transform.created = transform.created_at.slice(0,16);
-    transform.profileImage = transform.user.profile_image_url_https;
-    transform.text = transform.text;
-    return transform;
-}   
+	// Need to convert the model to a JSON object
+	var transform = model.toJSON();
+	transform.userName = transform.user.name;
+	transform.screenName = "@" + transform.user.screen_name;
+	transform.created = transform.created_at.slice(0, 16);
+	transform.profileImage = transform.user.profile_image_url_https;
+	transform.text = transform.text;
+	return transform;
+}
 
-$.tableView.addEventListener('close', function(){
+function showTweet(evt) {
+	Ti.API.info(evt);
+	Ti.API.info(evt.source);
+	Ti.API.info(JSON.stringify(evt.source));
+}
+
+$.tableView.addEventListener('close', function() {
 	Ti.API.info("Destroying twitter row");
 	$.destroy();
 });
