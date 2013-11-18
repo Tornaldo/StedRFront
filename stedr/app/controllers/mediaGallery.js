083621ab23459cf85ba596ea341c4e0330b18e5a
Odd Fredrik Mørch Rogstad <oddfredrikrogstad@gmail.com>
2013-11-18 19:51:05 +0100

    removed commented-out code

--- stedr/app/controllers/mediaGallery.js
+++ stedr/app/controllers/mediaGallery.js
@@ -46,53 +46,9 @@ if (videoUrlList) {
 		var wallVideo1 = Ti.Media.createVideoPlayer({
 			url : videoUrlList[i],
 			mediaControlStyle : Ti.Media.VIDEO_CONTROL_DEFAULT,
-			// scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
 			autoplay : true,
 		});
-
-		// var wallVideo2 = Titanium.Media.createVideoPlayer({
-		// url : videoUrlList[i],
-		// backgroundColor : 'blue',
-		// movieControlStyle : Ti.Media.VIDEO_CONTROL_DEFAULT,
-		// scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FIT,
-		// autoplay : true,
-		// });
-		// var wallVideo3 = Ti.Media.createVideoPlayer({
-		// url : videoUrlList[i],
-		// backgroundColor : 'blue',
-		// movieControlMode : Ti.Media.VIDEO_CONTROL_DEFAULT,
-		// scalingMode : Titanium.Media.VIDEO_SCALING_MODE_FILL,
-		// autoplay : true,
-		// });
-		// var wallVideo4 = Ti.Media.createVideoPlayer({
-		// url : videoUrlList[i],
-		// backgroundColor : 'blue',
-		// mediaControlStyle : Ti.Media.VIDEO_CONTROL_EMBEDDED,
-		// // scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
-		// autoplay : true,
-		// });
-		// var wallVideo5 = Ti.Media.createVideoPlayer({
-		// url : videoUrlList[i],
-		// backgroundColor : 'blue',
-		// movieControlStyle : Ti.Media.VIDEO_CONTROL_VOLUME_ONLY,
-		// // scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
-		// autoplay : true,
-		// });
-		// var wallVideo6 = Ti.Media.createVideoPlayer({
-		// url : videoUrlList[i],
-		// backgroundColor : 'blue',
-		// movieControlMode : Ti.Media.VIDEO_CONTROL_VOLUME_ONLY,
-		// // scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
-		// autoplay : true,
-		//
-		// });
-
 		$.mediaScroller.addView(wallVideo1);
-		// $.mediaScroller.addView(wallVideo2);
-		// $.mediaScroller.addView(wallVideo3);
-		// $.mediaScroller.addView(wallVideo4);
-		// $.mediaScroller.addView(wallVideo5);
-		// $.mediaScroller.addView(wallVideo6);
 	}
 }
 
